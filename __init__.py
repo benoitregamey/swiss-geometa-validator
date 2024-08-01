d7f8eb34df1a2b9f38a0bd768855c423431ae433
“benoitregamey” <benoitregamey@hotmail.com>
2024-08-01 22:16:47 +0200

    improve config by instance

--- __init__.py
+++ __init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
+import os 
 
 def create_app():
     """Create Flask application."""
     app = Flask(__name__, instance_relative_config=False)
 
     # configure app using the Config class defined in config.py
-    app.config.from_object('config.FlaskConfig')
+    if os.environ.get("FLASK_ENV") == "development":
+        app.config.from_object('config.DevelopmentConfig')
+    else:
+        app.config.from_object('config.ProductionConfig')
 
     with app.app_context():
 
