40fa3fa7c28016e8db339a7737f3093c2b2df6d2
“benoitregamey” <benoitregamey@hotmail.com>
2024-10-04 12:38:14 +0200

    improve CI

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 class ProductionConfig:
     ENV = "production"
     DEBUG = False
-    SECRET_KEY = os.urandom(24)    
+    SECRET_KEY = os.urandom(24)
 
 class DevelopmentConfig(ProductionConfig):
     ENV = "development"
